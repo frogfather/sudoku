550a5121d72e70fe7107bf0a3ad9086f5e2a3736
frogfather <j.a.campbell@icloud.com>
2022-01-30 12:00:08 +0000

    add sudokuNumber class

--- backup/cell.pas
+++ backup/cell.pas
@@ -7,6 +7,30 @@ interface
 uses
   Classes, SysUtils,arrayUtils;
 type
+
+  { TSudokuNumber }
+  TSudokuNumber = class(TInterfacedObject)
+    private
+    fValue:integer;
+    //set by number itself if it is being used in a calculation
+    fUsedInCalc:boolean;
+    //is the number available either for display or calculation
+    fAvailable:boolean;
+    //If another number has signalled that it is used in a calculation
+    //and constraints prevent this one being used in the calculation.
+    fExclude:boolean;
+    //What cell does this belong to?
+    fParent:TObject;
+    //Properties are all accessible by the containing cell only
+    protected
+    property parent: TObject read fParent;
+    property usedInCalc: boolean read fUsedInCalc write fUsedInCalc;
+    property available: boolean read fAvailable write fAvailable;
+    property exclude: boolean read fExclude write fExclude;
+    property value: integer read fValue;
+    public
+    constructor create(aOwner:TObject;initValue:integer=-1);
+  end;
   
   { TCell }
 
@@ -20,7 +44,11 @@ type
     fCentreMarks: TIntArray;
     fCandidates: TIntArray;
     public
-    constructor create(row, column, box: integer; candidates:TIntArray;value: integer=-1);
+    constructor create(row, column, box: integer;
+      candidates:TIntArray;
+      edgeMarks: TIntArray=nil;
+      centreMarks:TIntArray=nil;
+      value: integer=-1);
     procedure setValue(newValue:integer);
     procedure updateEdgeMarks(newValues:TIntArray);
     procedure updateCentreMarks(newValues:TIntArray);
@@ -38,14 +66,31 @@ type
 
 implementation
 
+{ TSudokuNumber }
+
+constructor TSudokuNumber.create(aOwner:TObject;initValue: integer);
+begin
+  fParent:=aOwner;
+  fValue:= value;
+  fUsedInCalc:= false;
+  fAvailable:= true;
+  fExclude:= false;
+end;
+
 { TCell }
 
-constructor TCell.create(row, column, box: integer; candidates:TIntArray;value: integer=-1);
+constructor TCell.create(row, column, box: integer;
+  candidates:TIntArray;
+  edgeMarks: TIntArray=nil;
+  centreMarks:TIntArray=nil;
+  value: integer=-1);
 begin
   fRow:=row;
   fColumn:=column;
   fBox:=box;
   fCandidates:=candidates;
+  fcentreMarks:= centreMarks;
+  fEdgeMarks:= edgeMarks;
   fValue:=value;
 end;
 
--- cell.pas
+++ cell.pas
@@ -7,6 +7,30 @@ interface
 uses
   Classes, SysUtils,arrayUtils;
 type
+
+  { TSudokuNumber }
+  TSudokuNumber = class(TInterfacedObject)
+    private
+    fValue:integer;
+    //set by number itself if it is being used in a calculation
+    fUsedInCalc:boolean;
+    //is the number available either for display or calculation
+    fAvailable:boolean;
+    //If another number has signalled that it is used in a calculation
+    //and constraints prevent this one being used in the calculation.
+    fExclude:boolean;
+    //What cell does this belong to?
+    fParent:TObject;
+    //Properties are all accessible by the containing cell only
+    protected
+    property parent: TObject read fParent;
+    property usedInCalc: boolean read fUsedInCalc write fUsedInCalc;
+    property available: boolean read fAvailable write fAvailable;
+    property exclude: boolean read fExclude write fExclude;
+    property value: integer read fValue;
+    public
+    constructor create(aOwner:TObject;initValue:integer=-1);
+  end;
   
   { TCell }
 
@@ -41,6 +65,17 @@ type
   TGameArray = array of array of TCell;
 
 implementation
+
+{ TSudokuNumber }
+
+constructor TSudokuNumber.create(aOwner:TObject;initValue: integer);
+begin
+  fParent:=aOwner;
+  fValue:= initValue;
+  fUsedInCalc:= false;
+  fAvailable:= true;
+  fExclude:= false;
+end;
 
 { TCell }
 
